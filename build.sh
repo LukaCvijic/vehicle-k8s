cb755b781a9e49939865f4d84be0797a9a8143b1
LukaCvijic <lukacvijic@outlook.com>
2024-03-05 12:12:56 +0100

    aaaaaaa

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
 docker build --tag ghcr.io/$GITHUB_ACTOR/backend:latest ./target
 docker push ghcr.io/$GITHUB_ACTOR/backend:latest
+
